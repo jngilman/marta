7d1dbcb066f469cdfba1daee74022715f0660e90
James Gilman <jngilman@mac.com>
2016-02-11 10:30:51 -0500

    Front end work through show page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,4 @@
 class LocationsController < ApplicationController
-  include LocationsHelper
 
   before_action :set_location, only: [:show, :edit, :update, :destroy]
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -4,7 +4,7 @@ class Location < ActiveRecord::Base
 
 	def self.nearby(lng1, lat1, lng2, lat2)
 
-		if (lng1-lng2).abs <= 0.01 && (lat1-lat2).abs <= 0.01
+		if (lng1-lng2).abs <= 0.005 && (lat1-lat2).abs <= 0.005
 			return true 
 		else
 			return false
