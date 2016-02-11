fe9f4d5da803f00fa4b797e4a6bc611716ab4982
James Gilman <jngilman@mac.com>
2016-02-11 10:09:53 -0500

    Started app to show APIs

---
+++ app/models/location.rb
@@ -0,0 +1,20 @@
+class Location < ActiveRecord::Base
+	geocoded_by :my_location
+	after_validation :geocode
+
+	def self.nearby(lng1, lat1, lng2, lat2)
+
+		if (lng1-lng2).abs <= 0.01 && (lat1-lat2).abs <= 0.01
+			return true 
+		else
+			return false
+		end 
+	end
+
+
+	def my_location
+		"#{address}, #{city}, GA"
+	end
+
+
+end
